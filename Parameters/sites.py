9d0cb264a373e7d837281bfd59324a2e43c61636
epoirierIRD <etienne.poirier@ird.fr>
2026-01-13 16:51:06 +0100

    updated proc and sites files

--- Parameters/sites.py
+++ Parameters/sites.py
@@ -6,8 +6,11 @@ Created on Thu Jun 19 11:21:52 2025
 @author: epoirier
 """
 
-# Site IDs and correponding latitudes, dictionnary
+# Site IDs and correponding latitudes, names, list of dictionnaries
 
+'''
+
+#
 site_latitudes = {
     1: 43.523, # Site 1: Marseille
     2: 45.762, # Site 2: Arcachon
@@ -15,6 +18,24 @@ site_latitudes = {
     4: 48.745, # Site 4: Roscoff
     5: 48.350  # Site 5: Plouzane
 }
+#
+
 # how to use
 # latitude = site_latitudes[5]
 # print(latitude)  # Output: 48.383
+
+'''
+sites = [
+    {"id": 1, "station": "Wimereux", "latitude": 50.6875}, # Point C
+    {"id": 3, "station": "Roscoff", "latitude": 48.7778}, # Astan
+    {"id": 5, "station": "Brest", "latitude": 48.3589}, # Portzic
+    {"id": 6, "station": "Arcachon", "latitude": 44.6641}, # Eyrac
+    {"id": 7, "station": "Gironde", "latitude": 45.5167},# pk 86
+    {"id": 10, "station": "Banyuls", "latitude": 42.4883}, # Sola
+    {"id": 11, "station": "Marseille", "latitude": 43.2417}, # Frioul
+    {"id": 12, "station": "Villefranche", "latitude": 43.6833}, # Point B
+    {"id": 17, "station": "Luc-sur-Mer", "latitude": 49.3188}, # Luc-sur-Mer
+    {"id": 18 , "station": "La Rochelle", "latitude": 46.0842}, # Antioche
+    {"id": 19, "station": "Dinard", "latitude": 48.6333}, # Bizeux
+    {"id": 22 , "station": "Sete", "latitude": 43.3267}, # Sete
+    ]
